5cdef793e853bc7d1762e96d7c9f11f99c72283d
Felipe Humberto Teixeira <felipe@dinfo.com.br>
2020-07-10 10:29:36 -0300

    feat: add text and button components

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import { Image } from 'react-native';
 
 import logoImg from '../../assets/logo.png';
+import Button from '../../components/Button';
+import Input from '../../components/Input';
+
 import { Container, Title } from './styles';
 
 const SignIn: React.FC = () => {
@@ -9,6 +12,9 @@ const SignIn: React.FC = () => {
     <Container>
       <Image source={logoImg} />
       <Title>Faça seu logon</Title>
+      <Input name="email" icon="mail" placeholder="E-mail" />
+      <Input name="password" icon="lock" placeholder="Senha" />
+      <Button>Entrar</Button>
     </Container>
   );
 };
